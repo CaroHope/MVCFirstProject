6e81660a53616906320399f853436b63e88df70c
Evelyn Carolina <evecarojorge@gmail.com>
2022-09-21 13:44:54 -0400

    Commit Inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdministracionEmpresaMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/MyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AdministracionEmpresaMVC.Models
+{
+    public class MyDbContext:DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) :
+            base(options)
+        {
+
+        }
+    }
+}
